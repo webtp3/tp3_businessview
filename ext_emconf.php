705c81a447a99301fdc39a1b6363bf63f06f4131
tudy <email@thomasruta.de>
2018-08-26 08:03:20 +0200

    merges github to bitbucket

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '0.3.4',
+    'version' => '0.3.5',
     'constraints' =>
         array (
             'depends' =>
