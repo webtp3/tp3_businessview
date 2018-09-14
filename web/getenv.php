dac4bdcc40bb378822ec6074ebe28728ff1e1712
tudy <email@thomasruta.de>
2018-09-14 16:52:34 +0200

    [fix] praxis demo

--- web/getenv.php
+++ web/getenv.php
@@ -1,7 +1,7 @@
 <?php
 
 // include composer's autoload file to make .env available in $_ENV
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/private/Build/vendor/autoload.php';
 
 # output some basic info about host - needs to be set in .env file
 echo 'HOST_TITLE: ' . $_ENV['HOST_TITLE']; 
