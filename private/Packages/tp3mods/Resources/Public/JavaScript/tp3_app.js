5805ba314708d46721db3b5874ccf33294f60a7c
tudy <email@thomasruta.de>
2018-10-20 12:16:02 +0200

    [fix] responsive Images

--- private/Packages/tp3mods/Resources/Public/JavaScript/tp3_app.js
+++ private/Packages/tp3mods/Resources/Public/JavaScript/tp3_app.js
@@ -139,9 +139,9 @@ var init = false;
 var show, go, scoll_pos;
 var scroll_pos = $j(document).scrollTop(),
     headerheight = $j('header.navbar-top').height(),
-    headerwidth = $j('header').first().width(),
-    logoheight = $j('.logo').first().height(),
-    logowidth = $j('.logo').first().width(),
+    headerwidth = $j('header').width(),
+    logoheight = headerheight * 0.9,
+    logowidth = $j('.navbar-brand-image').width(),
     toolbarheight = $j('.toolbar').first().height();
 if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
     || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) mobile = true;
@@ -170,7 +170,7 @@ else
                 $j('.toolbar .frame').css({padding:"16px 0"});
                 $j(this).toggleClass('anim');
                 $j('header.navbar-top').removeClass("flat");
-                $j(' a.navbar-brand-image, #logo, .logo').width(logowidth).height(logoheight);
+                $j(' a.navbar-brand-image, #logo, .logo').height(logoheight).css({width:"auto"});
                 $j('.navbar-collapse:not(.in) .nav > li > a, .headerslogan').css({"line-height": (headerheight - toolbarheight) * 0.4  -0.5 +"px"});
                 //$j('.headerslogan').css({"padding-left":"140px"});
 
@@ -582,6 +582,214 @@ $.fn.serializeWithChkBox = function() {
 }
 
 var canvasDots = function() {
+    /*
+
+jQuery(window).load(function() {
+
+    var w_width, w_height, largeHeader, canvas, ctx, points, target, animateHeader = true;
+
+    // Main
+    largeHeader = jQuery( '.home #msb-tweenlite-header');
+    if ( jQuery( largeHeader ).height() > 0 ) {
+       initHeader();
+       initAnimation();
+       addListeners();
+    } else { return; }
+
+    function initHeader() {
+
+        w_height = jQuery( largeHeader ).height();
+        w_width  = jQuery( largeHeader ).width();
+        target = {x: w_width/2, y: w_height/2};
+        // console.log( w_height);
+
+        canvas = jQuery('<canvas/>',{ 'class':'radHuh', id: 'msb_canvas_314'
+                }).prop({
+                    width: w_width,
+                    height: w_height
+                });
+        jQuery('#msb-tweenlite-header .et-pb-active-slide .et_parallax_bg').prepend(canvas);
+
+        canvas.width = w_width;
+        canvas.height = w_height;
+        ctx = canvas[0].getContext('2d');
+
+        // create points
+        points = [];
+        for(var x = 0; x < w_width; x = x + w_width/15) {
+            for(var y = 0; y < w_height; y = y + w_height/15) {
+                var px = x + Math.random()*w_width/15;
+                var py = y + Math.random()*w_height/15;
+                var p = {x: px, originX: px, y: py, originY: py };
+                points.push(p);
+            }
+        }
+
+        // for each point find the 5 closest points
+        for(var i = 0; i < points.length; i++) {
+            var closest = [];
+            var p1 = points[i];
+            for(var j = 0; j < points.length; j++) {
+                var p2 = points[j]
+                if(!(p1 == p2)) {
+                    var placed = false;
+                    for(var k = 0; k < 5; k++) {
+                        if(!placed) {
+                            if(closest[k] == undefined) {
+                                closest[k] = p2;
+                                placed = true;
+                            }
+                        }
+                    }
+
+                    for(var k = 0; k < 5; k++) {
+                        if(!placed) {
+                            if(getDistance(p1, p2) < getDistance(p1, closest[k])) {
+                                closest[k] = p2;
+                                placed = true;
+                            }
+                        }
+                    }
+                }
+            }
+            p1.closest = closest;
+        }
+
+        // assign a circle to each point
+        for(var i in points) {
+            //rgba(255,255,255,0.7)
+            var c = new Circle(points[i], 2+Math.random()*2, 'rgba(116,199,168,0.7)');
+            points[i].circle = c;
+        }
+    }
+
+    // Event handling
+    function addListeners() {
+        if(!('ontouchstart' in window)) {
+            window.addEventListener('mousemove', mouseMove);
+        }
+        window.addEventListener('scroll', scrollCheck);
+        window.addEventListener('resize', resize);
+    }
+
+    function mouseMove(e) {
+        var posx = posy = 0;
+        if (e.pageX || e.pageY) {
+            posx = e.pageX;
+            posy = e.pageY;
+        }
+        else if (e.clientX || e.clientY)    {
+            posx = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+            posy = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+        }
+        target.x = posx;
+        target.y = posy;
+    }
+
+    function scrollCheck() {
+        if(document.body.scrollTop > w_height) animateHeader = false;
+        else animateHeader = true;
+    }
+
+    function resize() {
+        // largeHeader.style.height = w_height+'px';
+        largeHeader = jQuery( '.home #msb-tweenlite-header');
+        w_height = jQuery( largeHeader ).height();
+        w_width  = jQuery( largeHeader ).width();
+        target = {x: w_width/2, y: w_height/2};
+
+        jQuery(canvas).prop({
+                    width: w_width,
+                    height: w_height
+                });
+        canvas.width = w_width;
+        canvas.height = w_height;
+        ctx = canvas[0].getContext('2d');
+
+
+    }
+
+    // animation
+    function initAnimation() {
+        animate();
+        for(var i in points) {
+            shiftPoint(points[i]);
+        }
+    }
+
+    function animate() {
+        if(animateHeader) {
+            ctx.clearRect(0,0,w_width,w_height);
+            for(var i in points) {
+                // detect points in range
+                if(Math.abs(getDistance(target, points[i])) < 4000) {
+                    points[i].active = 0.3;
+                    points[i].circle.active = 0.7;
+                } else if(Math.abs(getDistance(target, points[i])) < 20000) {
+                    points[i].active = 0.1;
+                    points[i].circle.active = 0.3;
+                } else if(Math.abs(getDistance(target, points[i])) < 40000) {
+                    points[i].active = 0.02;
+                    points[i].circle.active = 0.1;
+                } else {
+                    points[i].active = 0;
+                    points[i].circle.active = 0;
+                }
+
+                drawLines(points[i]);
+                points[i].circle.draw();
+            }
+        }
+        requestAnimationFrame(animate);
+    }
+
+    function shiftPoint(p) {
+        TweenLite.to(p, 1+1*Math.random(), {x:p.originX-50+Math.random()*100,
+            y: p.originY-50+Math.random()*100, ease:Circ.easeInOut,
+            onComplete: function() {
+                shiftPoint(p);
+            }});
+    }
+
+    // Canvas manipulation
+    function drawLines(p) {
+        if(!p.active) return;
+        for(var i in p.closest) {
+            ctx.beginPath();
+            ctx.moveTo(p.x, p.y);
+            ctx.lineTo(p.closest[i].x, p.closest[i].y);
+            ctx.strokeStyle = 'rgba(255,255,255,'+ p.active+')';
+            ctx.stroke();
+        }
+    }
+
+    function Circle(pos,rad,color) {
+        var _this = this;
+
+        // constructor
+        (function() {
+            _this.pos = pos || null;
+            _this.radius = rad || null;
+            _this.color = color || null;
+        })();
+
+        this.draw = function() {
+            if(!_this.active) return;
+            ctx.beginPath();
+            ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
+            ctx.fillStyle = 'rgba(116,199,168,'+ _this.active+')';
+            ctx.fill();
+        };
+    }
+
+    // Util
+    function getDistance(p1, p2) {
+        return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
+    }
+
+});
+
+     */
     //   var canvas = $j('.connecting-dots').length < 1 ? $j('<canvas class="connecting-dots"></canvas>').prependTo($('#content').first()).hide() : $j('.connecting-dots');
     var canvas = document.querySelector('canvas'),
         ctx = canvas.getContext('2d'),
