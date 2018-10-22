45b129a895665d13bab0dff91bb0432bf682d810
tudy <email@thomasruta.de>
2018-10-22 23:16:31 +0200

    [fix] RIchSnippets tp3mods [fix] tt_address

--- private/Packages/tt_address/Classes/Controller/LegacyPluginController.php
+++ private/Packages/tt_address/Classes/Controller/LegacyPluginController.php
@@ -501,6 +501,7 @@ class LegacyPluginController extends AbstractPlugin
         if ($templateFile === 'default') {
             $templateFile = $this->conf['defaultTemplateFileName'];
         }
+
         $templateCode = file_get_contents(GeneralUtility::getFileAbsFileName($this->conf['templatePath'] . $templateFile));
         return $this->cObj->getSubpart($templateCode, '###TEMPLATE_ADDRESS###');
     }
