87aca397920980132ba9cc867b529ee66eee6730
tudy <email@thomasruta.de>
2018-12-11 21:40:16 +0100

    [fix] tp3_openhours

---
+++ private/Packages/tp3_openhours/Classes/Domain/Model/OpenHour.php
@@ -0,0 +1,147 @@
+<?php
+namespace Tp3\Tp3Openhours\Domain\Model;
+
+/***
+ *
+ * This file is part of the "tt_address OpenHours" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ *  (c) 2018 Thomas Ruta &lt;email@thomasruta.de>, tp3
+ *
+ ***/
+
+/**
+ * OpenHour
+ */
+class OpenHour extends \TYPO3\CMS\Extbase\DomainObject\AbstractValueObject
+{
+
+    /**
+     * DayArray
+     *
+     * @var array
+     */
+    protected $DayArray =  ['','Mo','Di','Mi','Do','Fr','Sa','So','Mo-Fr','Sa-So','24x7'];
+    /*
+        ['Mo'=> 1],
+        ['Di'=> 2],
+        ['Mi'=> 3],
+        ['Do'=> 4],
+        ['Fr'=> 5],
+        ['Sa'=> 6],
+        ['So'=> 7],
+        ['Mo-Fr'=> 8],
+        ['Sa-So'=> 9],*/
+
+    /**
+     * Day of OpenHours
+     * 
+     * @var int
+     * @validate NotEmpty
+     */
+    protected $day = 0;
+
+    /**
+     * openTime
+     * 
+     * @var int
+     * @validate NotEmpty
+     */
+    protected $openTime = 0;
+
+    /**
+     * closeTime
+     * 
+     * @var int
+     */
+    protected $closeTime = 0;
+    /**
+     * Returns the day
+     *
+     * @return int $day
+     */
+    public function getDayNames()
+    {
+        $timestamp = strtotime('next Sunday');
+        $days = array();
+        for ($i = 0; $i < 7; $i++) {
+            $days[] = strftime('%A', $timestamp);
+            $timestamp = strtotime('+1 day', $timestamp);
+        }
+        $this->DayArray = array_merge( $this->DayArray,$days);
+    }
+    /**
+     * Returns the day
+     * 
+     * @return int $day
+     */
+    public function getDay()
+    {
+        return $this->day;
+    }
+    /**
+     * Returns the dayname
+     *
+     * @return string
+     */
+    public function getDayName()
+    {
+
+        return $this->DayArray[$this->day];
+    }
+
+    /**
+     * Sets the day
+     * 
+     * @param int $day
+     * @return void
+     */
+    public function setDay($day)
+    {
+        $this->day = $day;
+    }
+
+    /**
+     * Returns the openTime
+     * 
+     * @return int $openTime
+     */
+    public function getOpenTime()
+    {
+        return $this->openTime;
+    }
+
+    /**
+     * Sets the openTime
+     * 
+     * @param int $openTime
+     * @return void
+     */
+    public function setOpenTime(int $openTime)
+    {
+        $this->openTime = $openTime;
+    }
+
+    /**
+     * Returns the closeTime
+     * 
+     * @return int $closeTime
+     */
+    public function getCloseTime()
+    {
+        return $this->closeTime;
+    }
+
+    /**
+     * Sets the closeTime
+     * 
+     * @param int $closeTime
+     * @return void
+     */
+    public function setCloseTime(int $closeTime)
+    {
+        $this->closeTime = $closeTime;
+    }
+}
---
+++ private/Packages/tp3_openhours/ext_emconf.php
@@ -0,0 +1,41 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "tp3_openhours".
+ *
+ * Auto generated 25-04-2018 15:46
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'tt_address OpenHours',
+  'description' => 'Öffnungszeiten für tt_address &amp; businessview',
+  'category' => 'misc',
+  'author' => 'Thomas Ruta',
+  'author_email' => 'email@thomasruta.de',
+  'state' => 'alpha',
+  'uploadfolder' => false,
+  'createDirs' => '',
+  'clearCacheOnLoad' => 0,
+  'version' => '0.0.6',
+  'constraints' => 
+  array (
+'depends' =>
+    array (
+        'typo3' => '8.7.0-9.9.99',
+        'tt_address' => '*',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  'clearcacheonload' => false,
+  'author_company' => NULL,
+);
+
