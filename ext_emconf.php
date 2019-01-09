bc39470dec70843114e171e13cc07d126f0e8d49
tudy <email@thomasruta.de>
2019-01-08 23:06:42 -0800

    [set] 1.0.5 to dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,7 +17,7 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => '0',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.4',
+    'version' => '1.0.5',
     'constraints' =>
         [
             'depends' =>
