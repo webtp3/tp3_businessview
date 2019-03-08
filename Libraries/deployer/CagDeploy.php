bf7352780a07fa7b45faa992438a4e23509467ba
tudy <email@thomasruta.de>
2019-03-08 01:58:18 -0800

    [fix] deployer befor install [fix] cal to web-tp3/cal

--- Libraries/deployer/CagDeploy.php
+++ Libraries/deployer/CagDeploy.php
@@ -46,125 +46,125 @@ class CagDeploy {
      */
     public static function deploy($deploy_path = '.')
     {
-        require __DIR__ . '/../../Build/vendor/deployer/deployer/recipe/typo3.php';
-        $yaml = Yaml::parse(file_get_contents(__DIR__ . '/../../config/servers.yaml'));
-        $yamlString = Yaml::dump($yaml);
-        Deployer::set('repository', self::$repository);
-        Deployer::set('keep_releases',  self::$keep_releases);
-        Deployer::set('shared_dirs',  self::$shared_dirs);
-        Deployer::set('shared_files',  self::$shared_files);
-        Deployer::set('writable_dirs',  self::$writable_dirs);
-        Deployer::set('writable_use_sudo',  self::$writable_use_sudo); // Using sudo in writable commands?
-        /**
-         * Deploy start, prepare deploy directory
-         */
-    Deployer::task('deploy:start', function ()
-    {
-        cd('~');
-        run("if [ ! -d {{deploy_path}} ]; then mkdir -p {{deploy_path}}; fi");
-        cd('{{deploy_path}}');
-    })->setPrivate();
+        if (is_file(__DIR__ . 'recipe_typo3.php')) {
+            require __DIR__ . '/../../Build/vendor/deployer/deployer/recipe/typo3.php';
 
-        /**
-         * Deploy configure
-         */
-    desc('Make configure files for your stage');
-    Deployer::task('deploy:configure', function ()
-    {
-        /**
-         * Paser value for template compiler
-         *
-         * @param array $matches
-         * @return string
-         */
-        $paser = function ($matches) {
-            if (isset($matches[1])) {
-                $value = get($matches[1]);
-                if (is_null($value) || is_bool($value) || is_array($value)) {
-                    $value = var_export($value, true);
-                }
-            } else {
-                $value = $matches[0];
-            }
-            return $value;
-        };
+            $yaml = Yaml::parse(file_get_contents(__DIR__ . '/../../config/servers.yaml'));
+            $yamlString = Yaml::dump($yaml);
+            Deployer::set('repository', self::$repository);
+            Deployer::set('keep_releases', self::$keep_releases);
+            Deployer::set('shared_dirs', self::$shared_dirs);
+            Deployer::set('shared_files', self::$shared_files);
+            Deployer::set('writable_dirs', self::$writable_dirs);
+            Deployer::set('writable_use_sudo', self::$writable_use_sudo); // Using sudo in writable commands?
+            /**
+             * Deploy start, prepare deploy directory
+             */
+            Deployer::task('deploy:start', function () {
+                cd('~');
+                run("if [ ! -d {{deploy_path}} ]; then mkdir -p {{deploy_path}}; fi");
+                cd('{{deploy_path}}');
+            })->setPrivate();
 
-        /**
-         * Template compiler
-         *
-         * @param string $contents
-         * @return string
-         */
-        $compiler = function ($contents) use ($paser) {
-            $contents = preg_replace_callback('/\{\{\s*([\w\.]+)\s*\}\}/', $paser, $contents);
-
-            return $contents;
-        };
-
-        $finder = new \Symfony\Component\Finder\Finder();
-        $iterator = $finder
-            ->files()
-            ->name('*.tpl')
-            ->in(__DIR__ . '/../../config');
-        $tmpDir = sys_get_temp_dir();
-        /* @var $file \Symfony\Component\Finder\SplFileInfo */
-        foreach ($iterator as $file) {
-            $success = false;
-            // Make tmp file
-            $tmpFile = tempnam($tmpDir, 'tmp');
-            if (!empty($tmpFile)) {
-                try {
-                    $contents = $compiler($file->getContents());
-                    $target = preg_replace('/\.tpl$/', '', $file->getRelativePathname());
-                    // Put contents and upload tmp file to server
-                    if (file_put_contents($tmpFile, $contents) > 0) {
-                        //run('mkdir -p {{deploy_path}}/shared/' . dirname($target));
-                        upload($tmpFile, '{{deploy_path}}/shared/' . $target);
-                        $success = true;
+            /**
+             * Deploy configure
+             */
+            desc('Make configure files for your stage');
+            Deployer::task('deploy:configure', function () {
+                /**
+                 * Paser value for template compiler
+                 *
+                 * @param array $matches
+                 * @return string
+                 */
+                $paser = function ($matches) {
+                    if (isset($matches[1])) {
+                        $value = get($matches[1]);
+                        if (is_null($value) || is_bool($value) || is_array($value)) {
+                            $value = var_export($value, true);
+                        }
+                    } else {
+                        $value = $matches[0];
                     }
-                } catch (\Exception $e) {
-                    //throw new $e;
+                    return $value;
+                };
+
+                /**
+                 * Template compiler
+                 *
+                 * @param string $contents
+                 * @return string
+                 */
+                $compiler = function ($contents) use ($paser) {
+                    $contents = preg_replace_callback('/\{\{\s*([\w\.]+)\s*\}\}/', $paser, $contents);
+
+                    return $contents;
+                };
+
+                $finder = new \Symfony\Component\Finder\Finder();
+                $iterator = $finder
+                    ->files()
+                    ->name('*.tpl')
+                    ->in(__DIR__ . '/../../config');
+                $tmpDir = sys_get_temp_dir();
+                /* @var $file \Symfony\Component\Finder\SplFileInfo */
+                foreach ($iterator as $file) {
                     $success = false;
+                    // Make tmp file
+                    $tmpFile = tempnam($tmpDir, 'tmp');
+                    if (!empty($tmpFile)) {
+                        try {
+                            $contents = $compiler($file->getContents());
+                            $target = preg_replace('/\.tpl$/', '', $file->getRelativePathname());
+                            // Put contents and upload tmp file to server
+                            if (file_put_contents($tmpFile, $contents) > 0) {
+                                //run('mkdir -p {{deploy_path}}/shared/' . dirname($target));
+                                upload($tmpFile, '{{deploy_path}}/shared/' . $target);
+                                $success = true;
+                            }
+                        } catch (\Exception $e) {
+                            //throw new $e;
+                            $success = false;
+                        }
+                        // Delete tmp file
+                        unlink($tmpFile);
+                    }
+                    if ($success) {
+                        writeln(sprintf("<info>✔</info> %s", $file->getRelativePathname()));
+                    } else {
+                        writeln(sprintf("<fg=red>✘</fg=red> %s", $file->getRelativePathname()));
+                    }
                 }
-                // Delete tmp file
-                unlink($tmpFile);
-            }
-            if ($success) {
-                writeln(sprintf("<info>✔</info> %s", $file->getRelativePathname()));
-            } else {
-                writeln(sprintf("<fg=red>✘</fg=red> %s", $file->getRelativePathname()));
-            }
+            });
+
+            /**
+             * Main task
+             */
+            Deployer::task('deploy', ['deploy:prepare',
+                'deploy:lock',
+                'deploy:release',
+                'deploy:update_code',
+                'deploy:shared',
+                'deploy:vendors',
+                'deploy:clear_paths',
+                'deploy:symlink',
+                'deploy:unlock',
+                'cleanup',])->desc('Deploy your project');
+
+            before('deploy:configure', 'deploy:start');
+            after('deploy:failed', 'deploy:unlock');
+            after('deploy:shared', 'deploy:writable');
+            before('deploy', 'deploy:start');
+            after('deploy', 'success');
+
+            /**
+             * Load stage and list server
+             */
+            //foreach (glob(__DIR__ . '/stage/*.php') as $filename) {
+            //    include $filename;
+            //}
+            serverList(__DIR__ . '/../../config/servers.yml');
         }
-    });
 
-        /**
-         * Main task
-         */
-    Deployer::task('deploy', ['deploy:prepare',
-    'deploy:lock',
-    'deploy:release',
-    'deploy:update_code',
-    'deploy:shared',
-    'deploy:vendors',
-    'deploy:clear_paths',
-    'deploy:symlink',
-    'deploy:unlock',
-    'cleanup',])->desc('Deploy your project');
-
-    before('deploy:configure', 'deploy:start');
-    after('deploy:failed', 'deploy:unlock');
-    after('deploy:shared', 'deploy:writable');
-    before('deploy', 'deploy:start');
-    after('deploy', 'success');
-
-        /**
-         * Load stage and list server
-         */
-    //foreach (glob(__DIR__ . '/stage/*.php') as $filename) {
-    //    include $filename;
-    //}
-    serverList(__DIR__ . '/../../config/servers.yml');
     }
-
-
 }
---
+++ ext_update.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+#after 7
+bin/typo3cms upgrade:all
+
+
+##ggf
+bin/typo3cms extension:activate realurl
+bin/typo3cms extension:activate tt_address
+bin/typo3cms extension:activate static_info_tables
+bin/typo3cms extension:activate static_info_tables_de
+bin/typo3cms extension:activate bootstrap_package
+bin/typo3cms extension:activate tp3mods
+bin/typo3cms extension:activate yoast_seo
+bin/typo3cms extension:activate wec_map
+bin/typo3cms extension:activate vidi
+bin/typo3cms extension:activate tp3ratings
+bin/typo3cms extension:activate tp3_social
+bin/typo3cms extension:activate tp3_openhours
+bin/typo3cms extension:activate tp3_news_extend
+bin/typo3cms extension:activate tp3_jobs
+bin/typo3cms extension:activate tp3_businessview
+bin/typo3cms extension:activate sr_feuser_register
+bin/typo3cms extension:activate news
+bin/typo3cms extension:activate media
+bin/typo3cms extension:activate gridelements
+bin/typo3cms extension:activate bootstrap_grids
+bin/typo3cms extension:activate dd_googlesitemap
+bin/typo3cms extension:activate bootstrap_grids
+bin/typo3cms extension:activate additional_reports
+bin/typo3cms extension:activate extractor
+
+bin/typo3cms extension:activate tp3_googlemaps
+bin/typo3cms extension:activate tp3_ddgooglesitemap_extend
+bin/typo3cms extension:activate tp3_hessenfilm
+bin/typo3cms extension:activate xqueue_subscribe
+bin/typo3cms extension:activate locationupdate
+bin/typo3cms extension:activate html5videoplayer
+bin/typo3cms extension:activate filmcomission
+
+
+bin/typo3cms language:update locationupdate
+
+
+bin/typo3cms language:update backend
+bin/typo3cms language:update beuser
+bin/typo3cms language:update core
+bin/typo3cms language:update dd_googlesitemap
+bin/typo3cms language:update extractor
+bin/typo3cms language:update feedit
+bin/typo3cms language:update form
+bin/typo3cms language:update gridelements
+bin/typo3cms language:update powermail
+bin/typo3cms language:update bootstrap_grids
+bin/typo3cms language:update bootstrap_package
+bin/typo3cms language:update tp3_businessview
+bin/typo3cms language:update sr_feuser_register
+bin/typo3cms language:update bootstrap_grids
+bin/typo3cms language:update tt_address
+bin/typo3cms language:update wec_map
+bin/typo3cms language:update yoast_seo
+bin/typo3cms language:update tp3_jobs
+bin/typo3cms language:update tp3_news_extend
+bin/typo3cms language:update tp3_facebook
+bin/typo3cms language:update tp3_openhours
+bin/typo3cms language:update tp3_social
+bin/typo3cms language:update setup
+bin/typo3cms language:update sys_action
+bin/typo3cms language:update tp3ratings
+bin/typo3cms language:update workspaces
+bin/typo3cms language:update media
+bin/typo3cms language:update news
+
