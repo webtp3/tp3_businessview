d2445e00e183b946f8f0df847a8dc627ffafcac0
tudy <email@thomasruta.de>
2019-04-14 06:06:16 +0200

    [fix] for wec

--- Packages/wec_map/ext_localconf.php
+++ Packages/wec_map/ext_localconf.php
@@ -26,10 +26,10 @@ if (!defined('TYPO3_MODE')) {
 ** add New CE-wizard elements
 */
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:wec_map/Configuration/PageTS/NewContentElementWizard.txt">'
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:wec_map/Configuration/PageTS/NewContentElementWizard.typoscript">'
 );
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = 'EXT:wec_map/Classes/Utility/Backend.php:Backend';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = \JBartels\WecMap\Utility\Backend::class;
 
 /* Add the Google geocoding service */
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService(
