151328d8a690550d7fa23b7e5c48ec1b66f3b94e
tudy <email@thomasruta.de>
2019-08-08 18:02:09 +0200

    [fix] news_extend layouts & isotope [fix} chromedriver tp3 [fix] tt_address style

--- Packages/tp3_news_extend/Classes/Hooks/HookHandler.php
+++ Packages/tp3_news_extend/Classes/Hooks/HookHandler.php
@@ -1,4 +1,11 @@
 <?php
+
+/*
+ * This file is part of the web-tp3/tp3_news_extend.
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
 namespace Tp3\Tp3NewsExtend\Hooks;
 
 /***************************************************************
@@ -32,14 +39,8 @@ namespace Tp3\Tp3NewsExtend\Hooks;
 
 class HookHandler {
 
-
-
-	function user_templateLayout(array &$confi) {
-
-
-
-
+    public function user_templateLayout(array &$confi)
+    {
     }
 
 }
-?>
