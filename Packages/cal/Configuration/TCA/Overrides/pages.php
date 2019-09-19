b6c26d87c3845b60336069e8761b254051302165
tudy <email@thomasruta.de>
2019-09-19 10:41:37 +0200

    [fix] pages override pagets

---
+++ Packages/cal/Configuration/TCA/Overrides/pages.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * This file is part of the web-tp3/yoast_cal.
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+
+// RTE Config (Old style)
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+    'cal',
+    'Configuration/PageTS/Page.tsconfig',
+    'EXT:cal :: PageTS.'
+);
