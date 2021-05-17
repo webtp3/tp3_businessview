13c1097da13a657bb77a3c7e64e8342a9959d30f
tudy <email@thomasruta.de>
2021-05-17 15:09:34 +0200

    - fix file stuff

--- Configuration/Extbase/Classes.php
+++ Configuration/Extbase/Classes.php
@@ -22,4 +22,9 @@ return [
             ],
         ],
     ],
+    \Tp3\Tp3Businessview\Domain\Model\File::class => [
+        'tableName' => 'sys_file',
+        'recordType' => \Tp3\Tp3Businessview\Domain\Model\File::class,
+
+    ],
 ];
